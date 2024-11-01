d6eb269b5779038573a8cddf8d2da74f0a0fd706
jack <144387193+failedsyntax@users.noreply.github.com>
2024-11-01 18:38:02 -0500

    Update LinkedList.h

--- LinkedList.h
+++ LinkedList.h
@@ -9,44 +9,39 @@ using namespace std;
 template<typename T>
 class List{
 private:
-    struct Node // https://stackoverflow.com/questions/15283195/argument-list-for-class-template-is-missing
-    {
-        Data<T> nodeValue;
-        Node* next;
+    Data<T>* Head;
+    Data<T>* Tail;
 
-        Node(const Data<T>& val) : nodeValue(val), next(NULL) {}
-    };
-
-    Node* Head;
-    Node* Tail;
     int numNodes;  // !!!!!!REMOVE IF NOT WANTING TO USE AT END!!!!!!
     
 public:
     //Constructor
-    List() : Head(NULL), Tail(NULL), numNodes(0) {}
+    List(){
+        Head = nullptr;
+        Tail = nullptr;
+        numNodes = 0;}
 
     ~List(){
-        Node* current = Head;
+        Data<T>* current = Head;
         while (current){
-            Node* nextNode = current->next;
+            Data<T>* nextNode = current->getNext();
             delete current;
             current = nextNode;
         }
         cout << "\nAll Nodes Removed" << endl;
     }
     // Mutators
-    void appendNode(const Data<Guest>& guest);
-    //void insertNode(Data<Guest>);
-    void deleteNode(Data<Guest>);
-    void pushNode(Data<Guest>);
-    void popNode(Data<Guest>&);
+    void appendNode(T);
+    // void deleteNode(T);
+    void pushNode(T);
+    void popNode(T);
 
     // Accessors
     void displayList();
 
     // Overloaded
     friend ostream& operator<<(ostream& stream, List& val) {
-        Node* current = val.Head;
+        Data<T>* current = val.Head;
         while (current){
             stream << current->nodeValue.getValue().getName() << " -> ";
             current = current -> next;
@@ -56,119 +51,73 @@ public:
         return stream;
     }
 };
-    template<typename T>
-    void List<T>::appendNode(const Data<Guest>& guest) // Bottom of list
-    {
-        Node *newNode;
-        newNode = new Node(guest);
 
-        if (!Head)
-        { // No head, default to both
-            Head = newNode;
-            Tail = newNode;
-        }
-        else
-        { // Otherwise push towards end
-            Tail->next = newNode;
-            Tail = newNode;
-        }
-        numNodes++;
-    };
-
-/*
-void List::insertNode(Data<Guest> guest)
-{
-    nodeValue *newNode;
-    nodeValue *nodePointer;
-    nodeValue *prevNode;
-
-    // New node with txt parameter
-    newNode = new nodeValue(guest);
-
-    if (!Head)
-    { // No head, make new node head
-        Head = newNode;
-        newNode->next = NULL;
-    }
-    else
-    {
-        nodePointer = Head;
-        prevNode = NULL;
-
-        // if nodePointer != end & node value is less than parameter, skip
-        while (nodePointer != NULL && nodePointer->nodeValue.getValue().getName() < guest.getValue().getName())
-        {
-            prevNode = nodePointer;
-            nodePointer = nodePointer->next;
-        };
-
-        // Is new node the start?
-        if (prevNode == NULL)
-        {
-            Head = newNode;
-            newNode->next = nodePointer;
-        }
-        else
-        {
-            prevNode->next = newNode;
-            newNode->next = nodePointer;
-        };
-    }
-};*/
 template<typename T>
-void List<T>::deleteNode(Data<Guest> guest)
+void List<T>::appendNode(T guest) // Bottom of list
 {
-    Node *nodePointer;
-    Node *prevNode;
+    Data<T>* newNode;
+    newNode = new Data<T>(guest);
 
-    // Is List empty?
-    if (!Head)
-    {
-        return;
-    };
+    newNode->setNext(Head);
+    Head = newNode;
 
-    // Is value to delete the head?
-    if (Head->nodeValue.getValue().getName() == guest.getValue().getName())
-    {
-        nodePointer = Head->next;
-        delete Head;
-        Head = nodePointer;
-    }
-    else
-    { // It's not..
-        nodePointer = Head;
-
-        // NodePointer != end and val of it isn't the val to delete
-        while (nodePointer != NULL && nodePointer->nodeValue.getValue().getName() != guest.getValue().getName())
-        {
-            prevNode = nodePointer;
-            nodePointer = nodePointer->next;
-        };
-
-        // does nodePointer even have a value
-        if (nodePointer)
-        {
-            if (nodePointer == Tail)
-            {
-                Tail = prevNode;
-            };
-
-            prevNode->next = nodePointer->next;
-            delete nodePointer;
-        }
-    }
+    numNodes++;
 };
 
+// template<typename T>
+// void List<T>::deleteNode(T guest)
+// {
+//     Data<T> *nodePointer;
+//     Data<T> *prevNode;
+
+//     // Is List empty?
+//     if (!Head)
+//     {
+//         return;
+//     }
+
+//     // Is value to delete the head?
+//     if (Head->nodeValue.getValue().getName() == guest.getValue().getName())
+//     {
+//         nodePointer = Head->next;
+//         delete Head;
+//         Head = nodePointer;
+//     }
+//     else
+//     { // It's not..
+//         nodePointer = Head;
+
+//         // NodePointer != end and val of it isn't the val to delete
+//         while (nodePointer != NULL && nodePointer->nodeValue.getValue().getName() != guest.getValue().getName())
+//         {
+//             prevNode = nodePointer;
+//             nodePointer = nodePointer->next;
+//         }
+
+//         // does nodePointer even have a value
+//         if (nodePointer)
+//         {
+//             if (nodePointer == Tail)
+//             {
+//                 Tail = prevNode;
+//             };
+
+//             prevNode->next = nodePointer->next;
+//             delete nodePointer;
+//         }
+//     }
+// };
+
 template<typename T>
-void List<T>::pushNode(Data<Guest> newGuest) { // Top of list
-    Node *nodePtr;
-    nodePtr = new Node(newGuest);
-    nodePtr->nodeValue = newGuest;
+void List<T>::pushNode(T newGuest) { // Top of list
+    Data<T> *nodePtr;
+    nodePtr = new Data<T>(newGuest);
+    nodePtr->setValue(newGuest);
 
     if (!Head) {
-        nodePtr->next = NULL;
+        nodePtr->setNext(NULL);
     } else {
-        nodePtr->next = Head;
+        nodePtr->setNext(Head);
     }
 
     Head = nodePtr;
@@ -176,8 +125,8 @@ void List<T>::pushNode(Data<Guest> newGuest) { // Top of list
 }
 
 template<typename T>
-void List<T>::popNode(Data<Guest>& newGuest) {
-    Node *ptr;
+void List<T>::popNode(T newGuest) {
+    Data<T> *ptr;
 
     if (Head) {
         numNodes--;
@@ -187,7 +136,7 @@ void List<T>::popNode(Data<Guest>& newGuest) {
             Head = Head->next;
         } else {
             Head = NULL;
-        };
+        }
 
         newGuest = ptr->nodeValue;
         delete ptr;
@@ -198,7 +147,7 @@ void List<T>::popNode(Data<Guest>& newGuest) {
 template<typename T>
 void List<T>::displayList()
 {
-    Node *nodePointer;
+    Data<T> *nodePointer;
     nodePointer = Head;
 
     while (nodePointer)
